59176818a9e2dcf2d0816cdf11e570cb7ae698fe
RomanVintoniak <romanvintoniak03@gmail.com>
2024-02-16 22:35:31 +0200

    Update CameraController

--- lab1/Maze/Assets/Scripts/CameraController.cs
+++ lab1/Maze/Assets/Scripts/CameraController.cs
@@ -10,18 +10,12 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
-        //offset = transform.position - player.transform.forward;
         transform.position = player.transform.position + shift;
     }
 
     void LateUpdate()
     {
-
-        //transform.position = player.transform.position + offset;
         transform.position = player.transform.position + shift;
         transform.rotation = player.transform.rotation;
     }
-
-    //var rotation = Quaternion.AngleAxis(30, new Vector3(20, 0, 0));
-    //transform.rotation = transform.rotation* rotation;
 }
